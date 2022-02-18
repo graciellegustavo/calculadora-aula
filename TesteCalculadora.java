55c4a81e8c787b09a2363cd3dcd0ba6072728111
Raphael Ramos <rar_aew@hotmail.com>
2022-02-18 18:55:54 -0300

    subtracao

--- TesteCalculadora.java
+++ TesteCalculadora.java
@@ -1,15 +1,20 @@
 public class TesteCalculadora {
 
 
-    public static Double soma (Double x,Double y) {
+    public static Double soma(Double x,Double y) {
         return x + y;
+    }
+
+    public static Double subtracao(Double x,Double y) {
+        return x - y;
     }
 
     public static void main(String[] args) {
         Double x = 5.0;
         Double y = 8.0;
 
-        System.out.println("soma =  " + soma(x, y));        
+        System.out.println("soma =  " + soma(x, y));  
+        System.out.println("subtracao =  " + subtracao(x, y));       
     }
     
 }
