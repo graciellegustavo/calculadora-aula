717e7807b19f08438bc7862c155b27f2f1280079
Raphael Ramos <rar_aew@hotmail.com>
2022-02-18 18:51:18 -0300

    soma

---
+++ TesteCalculadora.java
@@ -0,0 +1,15 @@
+public class TesteCalculadora {
+
+
+    public static Double soma (Double x,Double y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        Double x = 5.0;
+        Double y = 8.0;
+
+        System.out.println("soma =  " + soma(x, y));        
+    }
+    
+}
